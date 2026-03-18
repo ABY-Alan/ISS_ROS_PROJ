4e4c540ed52bab8edf88ddaee279d0f4b1772da1
Evelyn <e1553170@u.nus.edu>
2026-03-18 21:35:42 +0800

    add testing pipeline 0

---
+++ turtle_bot_ws/src/test0_all.py
@@ -0,0 +1,13 @@
+from test1_100_goals_empty_world import StartTest_EmptyWorld
+from test2_100_goals_box_world import StartTest_BoxWorld
+from test3_100_goals_door_world import StartTest_DoorWorld
+
+NUM_TRIALS_PER_TEST = 200
+
+StartTest_EmptyWorld("Model_1_PPO_Ckpt_Step_10000", NUM_TRIALS=NUM_TRIALS_PER_TEST)
+StartTest_EmptyWorld("Model_2_PPO_Ckpt_Step_10000", NUM_TRIALS=NUM_TRIALS_PER_TEST)
+StartTest_BoxWorld("Model_1_PPO_Ckpt_Step_10000", NUM_TRIALS=NUM_TRIALS_PER_TEST)
+StartTest_BoxWorld("Model_2_PPO_Ckpt_Step_10000", NUM_TRIALS=NUM_TRIALS_PER_TEST)
+StartTest_DoorWorld("Model_1_PPO_Ckpt_Step_10000", NUM_TRIALS=NUM_TRIALS_PER_TEST)
+StartTest_DoorWorld("Model_2_PPO_Ckpt_Step_10000", NUM_TRIALS=NUM_TRIALS_PER_TEST)
+
